4a99373d491c781ba3b4296c22f6a24496eb20d5
brianburridge <bburridg@gmail.com>
2014-09-23 12:58:23 -0400

    Added Assignment 5 and 6

---
+++ assignment_5/README.md
@@ -0,0 +1,2 @@
+# Assignment 5
+* Create an issue to read about [Symbols](http://www.troubleshooters.com/codecorn/ruby/symbols.htm) and [Literals](http://www.ruby-doc.org/core-2.1.1/doc/syntax/literals_rdoc.html)
---
+++ assignment_6/assignment_6.rb
@@ -0,0 +1,42 @@
+# Create a String variable named 'name'
+
+# Create a Date variable named "a_date"
+
+# Create an integer variable named "age"
+
+# Create an array of names named 'names'
+
+# Create another array, using %w, named 'more_names'
+
+# Create a Hash of names with ages named 'names_and_ages'
+
+# Keep this part as a way to test your code
+puts name.class
+puts a_date.class
+puts age.class
+puts names.class
+puts more_names.class
+puts names_and_ages.class
+
+puts name.is_a? String
+puts a_date.is_a? Date
+puts age.is_a? Fixnum
+puts names.is_a? Array
+puts more_names.is_a? Array
+puts names_and_ages.is_a? Hash
+
+# Create a String literal and print it's class (one line)
+
+# Create a Fixnum literal and print it's class (one line)
+
+# Create an Array literal and print it's class (one line)
+
+# Create a Hash literal and print it's class (one line). Note to keep from confusing Ruby you'll have to wrap what you are outputting in parenthesis
+
+# Create a symbol and print it's class
+
+# Create a String constant with any name you choose
+
+# Change the value of that constant
+
+# Print the constant's value to the screen
