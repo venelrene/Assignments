20404af16f179532e136ba9c9fb6bf2ddb52cfaa
venel <venelrene@gmail.com>
2014-09-23 13:44:35 -0400

    created Solution.rb #5

---
+++ solution.rb
@@ -0,0 +1,29 @@
+print "1. "
+puts 5 * 2
+
+print "2. "
+puts 3 < 4
+
+print "3. "
+puts (5 * 3) > 10
+
+print "4. "
+puts 2 == 3
+
+print "5. "
+puts (1 > 0) && (1 <= 1)
+
+print "6. "
+puts 2 == 2
+
+print "7. "
+puts (14 >= 10) || (14 <= 10)
+
+print "8. "
+puts 1 % 2 == 1
+
+print "9. "
+puts 1000 * 1
+
+print "10. "
+puts 400 / 100
