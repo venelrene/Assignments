b3f7d72dcfc6e91935452083e6ce5bccb26585f8
venel <venelrene@gmail.com>
2014-10-03 17:54:00 -0400

    deleted the extra new file it was using

--- assignment_16/solution_16.rb
+++ assignment_16/solution_16.rb
@@ -3,10 +3,9 @@ require 'net/http'
 # To get the uri from the website you desire
 uri = URI('http://www.scifiscripts.com/scripts/alien3_fasano.txt')
 # making a new file that i can write on
-new_web_file = File.new("solution_16.txt", "w+")
+file = File.new('solution_16.txt', 'a')
 # grabbing the uri and changing it to a string
 script_text = Net::HTTP.get(uri)
 # now its a string under a variable
-file = File.new('solution_16.txt', 'a')
 #save it in file
 file.write(script_text)
