0bed9bfeae460023ce8b3f3fba954db635d18037
venel <venelrene@gmail.com>
2014-10-04 12:26:05 -0400

    modified assignment_12

--- assignment_12/solution_12.rb
+++ assignment_12/solution_12.rb
@@ -2,7 +2,7 @@
 # Create an array of questions
 # Ask a question of the user, from the array
 # Continue asking questions until the user enters without giving an answer
-# When you run out of questions, start over
+# When you run out of gquestions, start over
 # For extra credit: Save all the answers with the questions, and print a summary at the end
 
 questions = ["What's your favorite color?", "What's your favorite genre?", "What's your favorite letter?"]
