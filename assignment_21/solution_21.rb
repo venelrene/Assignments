8abf7248e3aeb0228834b075c78ba59c8c3596a0
venel <venelrene@gmail.com>
2014-10-08 09:39:23 -0400

    modified not done assignment_21

--- assignment_21/solution_21.rb
+++ assignment_21/solution_21.rb
@@ -9,15 +9,15 @@ class Person
 
 #Create a public instance method called 'name' that takes the name and sets the first name and last name instance variables.
   def name(entered_name)
-    spilt_name = entered_name.spilt
-    @first_name = spilt_name[0].captial
-    @last_name = spilt_name[1].captial
+    split_name = entered_name.split
+    @first_name = split_name[0].capitalize
+    @last_name = split_name[1].capitalize
   end
 #Create a public instance method to get the user's age.
 
   def age
     puts "Enter age"
-    enter_age = gets.chomp.to_s
+    enter_age = gets.chomp.to_i
   end
 end
 #Ask the user their name as one question, but store it separately in first and last name  variables.
@@ -29,13 +29,14 @@ end
 new_user.name(entered_name)
 
 #After asking each user their information, output the user's age.
-user_age = user.age
+user_age = new_user.age
 
 ## Next
 =begin  !!!!! I need HELP with this PART BELOW
 Move the functionality in your age method into a private method called, "calculateAge". This method will be called from the public age method.
 
 Create a class variable that will count the number of users that enter in their information.
+ += 1
 After you output the user's age, also output the number of user they are so far (using the class variable).
 
 Loop to allow another user to enter their information. (exit when user enters \q for name). Create a User object for each user.
