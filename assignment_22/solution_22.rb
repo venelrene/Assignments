d1f94d3dba450aae6e6922a036541caeb1373bae
venel <venelrene@gmail.com>
2014-10-24 10:40:45 -0400

    assignment#22

--- assignment_22/solution_22.rb
+++ assignment_22/solution_22.rb
@@ -1,55 +1,43 @@
-
+require 'active_support/all'
 # Assignment 22
 # Create an InvoiceItem class and an Invoice class.
 # The class should have the following attributes: product_name, sale_price, quantity, tax percentage
-require 'active_support/all'
 
-  class InvoiceItem
-    attr_accessor :product_name, :sale_price, :quantity
-  end
+load 'invoice_class.rb'
 
-  class Invoice
-    attr_accessor :items, :tax_percentage
-  # Create a class method in Invoice that will calculate a total for an array of InvoiceItem objects
-  # Use reject or select methods in order to remove any InvoiceItem objects that have a zero quantity
-  # Use the sum method to get the total
-
-      def total_item
-        @items.reject {|item| item.quantity == 0}
-        invoice_total = @items.sum {|item|  item.sale_price * item.quantity}
-
-      end
-
-      def total
-        invoice_total = @items.sum {|item| item.sale_price * item.quantity * item.tax_percentage}
-      end
-  end
+  invoice = Invoice.new
 
 loop do
-# Ask the user to enter in the attributes for each invoice item, hitting ENTER on product name when done
-  new_invoice = InvoiceItem.new
+  # Ask the user to enter in the attributes for each invoice item, hitting ENTER on product name when done
   print "Please fillout the following (press ENTER to Exit)\n"
 
+  invoice_item = InvoiceItem.new
+
   print "Product Name:\n"
-  new_invoice.product_name = gets.chomp
-  break if new_invoice.product_name.length == 0
+  invoice_item.product_name = gets.chomp
+  break if invoice_item.product_name.length == 0
 
   print "Quantity:\n"
-  new_invoice.quantity = gets.chomp.to_i
-  break if new_invoice.quantity == 0
+  invoice_item.quantity = gets.chomp.to_i
+  break if invoice_item.quantity == 0
 
   print "Sale price:\n"
-  new_invoice.sale_price = gets.chomp.to_i.round(2)
+  invoice_item.sale_price = gets.chomp.to_i.round(2)
+
+  invoice.items << invoice_item
 
 end
 #Then output each line item.
-puts "Product requested:#{new_invoice.product_name}\n Price:#{new_invoice.sale_price}\n Quantity:#{new_invoice.quantity}\n"
+ invoice.items.each do |invoice_item|
+puts "Product requested: #{invoice_item.product_name}\n Price:#{invoice_item.sale_price}\n Quantity:#{invoice_item.quantity}\n"
+end
 
 #Show a sub-total without the tax, then a line item for the tax, then the total.
-invoice = Invoice.new
-sub_total = new_invoice.total_item
+sub_total = invoice.total_item
 puts "Sub Total:#{sub_total}"
 
-print "1% tax added to ALL items"
-tax = tax_percentage.gets.chomp.to_i
-total = sub_total + tax
+print "#{invoice.tax}= 7% tax added to items\n"
+# tax = invoice.tax
+
+total = invoice.total + sub_total
+puts "Final total with tax is: #{total}"
